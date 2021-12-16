f79551f66d1f875193d1a25ba56ab113fc4e1421
trudypainter <tpainter@mit.edu>
2021-12-16 08:14:15 -0500

    pip3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory
-app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app = Flask(__name__, static_url_path='', static_folder='./frontend/build')
 
 @app.route("/", defaults={'path':''})
 def serve(path):
