ce455da71358d6b26cf8f3c4708f16ba8ac802e6
trudypainter <tpainter@mit.edu>
2021-12-16 08:25:21 -0500

    heroku deploy

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, send_from_directory
-app = Flask(__name__, static_url_path='', static_folder='./frontend/build')
+app = Flask(__name__, static_folder='./frontend/build', static_url_path='')
 
-@app.route("/", defaults={'path':''})
-def serve(path):
-    return send_from_directory(app.static_folder,'index.html')
+@app.route('/', methods=["GET"])
+def index():
+    return app.send_static_file('index.html')
 
 
-@app.route('/api/get_listening')
-def get_listening():
-  return render_template('index.html')
+# @app.route('/api/get_listening')
+# def get_listening():
+#   return app.render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
