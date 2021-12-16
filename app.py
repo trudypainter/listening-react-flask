06c82523552dff4081b3777cccd20ea718a1cc24
trudypainter <tpainter@mit.edu>
2021-12-16 08:30:02 -0500

    cleaning

--- app.py
+++ app.py
@@ -3,12 +3,8 @@ app = Flask(__name__, static_folder='./frontend/build', static_url_path='')
 
 @app.route('/', methods=["GET"])
 def index():
+    print("GOT REQUEST *****😀")
     return app.send_static_file('index.html')
-
-
-# @app.route('/api/get_listening')
-# def get_listening():
-#   return app.render_template('index.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
