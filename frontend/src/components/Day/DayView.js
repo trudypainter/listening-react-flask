973b1c35737307078463716c6ce09d16bcab0dd4
trudypainter <tpainter@mit.edu>
2021-12-22 09:29:46 -0500

    updated readme

--- frontend/src/components/Day/DayView.js
+++ frontend/src/components/Day/DayView.js
@@ -1,7 +1,14 @@
+import React, { useState, useEffect } from "react";
 import "./DayView.css";
 
-const DayView = () => {
-  return <div className="dayView"></div>;
+const DayView = (props) => {
+  return (
+    <div className="dayView">
+      <DayMenu day={props.selectedDay} />
+      <DayChart day={props.selectedDay} />
+      <SongList day={props.selectedDay} />
+    </div>
+  );
 };
 
 export default DayView;
